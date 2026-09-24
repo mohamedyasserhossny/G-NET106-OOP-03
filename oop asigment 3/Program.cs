8c5098af8a60fe6eda8bc224a6887e5912bef690
mohamedyasserhossny <my226546@gmail.com>
2026-09-24 12:14:35 -0700

    question 1

--- oop asigment 3/Program.cs
+++ oop asigment 3/Program.cs
@@ -5,8 +5,13 @@
         static void Main(string[] args)
         {
             #region question 1
-            //a)overloading method: more than one method with the same name in the same class
-            //b)overriding : more than one method with the same name in diffrent class using virtual and overriding
+            //a)overloading method: more than one method with the same name in the same class,compile time
+            //overriding : more than one method with the same name in diffrent class using virtual and overriding,runtime
+            //b)static binding : know the method in compile time and known as early binding
+            // dynamic binding : know the method in run time and known as later binding
+            #endregion
+            #region question 2
+
             #endregion
         }
     }
